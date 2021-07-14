3040ff6b6f931cd19010c9a5903014d0bbbbd425
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-07-14 09:48:42 -0500

    Add Haunter animation to page load, Bootstrap, jQuery UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,13 @@ fetch(haunterData)
         haunterImg.setAttribute("src", data.sprites.front_default)
         console.log(data.sprites.front_default)
     })
+
+// ANIMATIONS
+
+function haunterHover() {
+    $("#haunter").effect("shake", {times:3, direction:"up", distance:5}, 9000)
+    haunterHover()
+}
+
+// RUN ON PAGE LOAD
+setTimeout(haunterHover, 100)
