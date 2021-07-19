b514fa49946e07ddfb7c4adadbce135917b9b895
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-07-19 07:40:17 -0500

    Adjust endgame functionality for playing again

--- assets/js/games.js
+++ assets/js/games.js
@@ -44,8 +44,9 @@ function gastlyInstructionsRemove() {
 
 // GASTLY MINI-GAME
 
-let catchSpaceEl = document.querySelector(".gastly-catch-space")
-let gastlyScore = document.querySelector("#gastly-score")
+const catchSpaceEl = document.querySelector(".gastly-catch-space")
+const mainPageEl = document.querySelector(".main")
+const gastlyScore = document.querySelector("#gastly-score")
 
 // display header
 function displayGastlyScoreboard () {
@@ -58,10 +59,11 @@ timer = 5;
 function gastlyTimer() {
     document.getElementById("timer").innerHTML = "Timer: " + timer;
     timer--;
-    if (timer < 0) {
-        catchSpaceEl.remove(); // NEED TO CHANGE THIS TO DISPLAY NONE, THEN, IN ENDGAME FUNCTIONS, TURN TO DISPLAY FLEX
+    if (timer <= -1) {
+        catchSpaceEl.remove(); 
+        timer = 5;
         gastlyScoreFinal();
-        return
+        return;
     }
     else {
         setTimeout(gastlyTimer, 1000);
@@ -76,36 +78,41 @@ var gastlyObj = {
 // gastly game function
 function gastlyGame() {
 
-    // source code: https://stackoverflow.com/questions/55668801/how-to-make-an-image-appear-on-a-random-position-onclick
-
-    var gastlyImg = document.querySelector(".gastly")
-
-    if (gastlyImg) {
-        gastlyImg.remove();
+    if (timer > 0) {
+        $(".gastly-catch-space").css("display", "flex");
+    
+        // source code: https://stackoverflow.com/questions/55668801/how-to-make-an-image-appear-on-a-random-position-onclick
+    
+        var gastlyImg = document.querySelector(".gastly")
+    
+        if (gastlyImg) {
+            gastlyImg.remove();
+        } else {
+            var img = document.createElement("img");
+            img.className = "gastly"
+            img.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png";
+            img.style.height = "22vh";
+            img.style.width = img.style.height;
+            img.addEventListener("click", () => {
+                gastlyObj.score = gastlyObj.score + 1
+                gastlyScore.textContent = gastlyObj.score
+                img.remove();
+            });
+            
+            // set the position
+            img.style.position = 'absolute';
+            img.style.top = (document.body.clientHeight - 200) * Math.random() + 'px';
+            img.style.left = (document.body.clientWidth - 200) * Math.random() + 'px';
+    
+            catchSpaceEl.append(img);
+            $(".gastly")
+            .effect("shake", {times: Math.ceil(Math.random() * 3), distance: (Math.ceil(Math.random) * 1000) + 5}, 1000)
+        }
+    
+        setTimeout(gastlyGame, ((Math.random() * 1000) + 200));
     } else {
-        var img = document.createElement("img");
-        img.className = "gastly"
-        img.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png";
-        img.style.height = "22vh";
-        img.style.width = img.style.height;
-        img.addEventListener("click", () => {
-            gastlyObj.score = gastlyObj.score + 1
-            gastlyScore.textContent = gastlyObj.score
-            img.remove();
-        });
-        
-        // set the position
-        img.style.position = 'absolute';
-        img.style.top = (document.body.clientHeight - 200) * Math.random() + 'px';
-        img.style.left = (document.body.clientWidth - 200) * Math.random() + 'px';
-
-        catchSpaceEl.append(img);
-        $(".gastly")
-        .effect("shake", {times: Math.ceil(Math.random() * 3), distance: (Math.ceil(Math.random) * 1000) + 5}, 1000)
+        return;
     }
-
-    setTimeout(gastlyGame, ((Math.random() * 1000) + 200));
-
 }
 
 function gastlyScoreFinal() {
@@ -115,19 +122,21 @@ function gastlyScoreFinal() {
     $(".gastly-score-final").text(gastlyObj.score);
 
     if (gastlyObj.score >= 1) {
-        $(".gastly-score-report-text-fail, .gastly-start-btn").css("display", "none");
+        $(".gastly-score-report-text-fail, .gastly-restart-btn").css("display", "none");
     } else {
         $(".gastly-score-report-text-success").css("display", "none")
         $(".text-entry-container").css("display", "none")
         let gastlyBtnEl = document.querySelector(".gastly-restart-btn")
         gastlyBtnEl.addEventListener("click", () => {
+            mainPageEl.append(catchSpaceEl)
+            $(".gastly").remove();
             $(".gastly-score-report").fadeOut(1500);
-            $(".gastly-catch-space").css("display", "flex");
+            // $(".gastly-catch-space").css("display", "flex");
             setTimeout(gastlyGame, 3000);
             setTimeout(displayGastlyScoreboard, 1600);
             setTimeout(gastlyTimer, 3000);
-            timer = 5;
             let timerContentEl = document.querySelector("#timer")
+            timer = 5
             timerContentEl.innerHTML = "Timer: " + timer;
         })
     }
